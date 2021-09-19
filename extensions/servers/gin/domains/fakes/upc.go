10ad857859265ecaac2cfafd4102d1d20c8718f6
vortex <d.a.viharev@gmail.com>
2021-09-19 21:02:25 +0300

    update

--- extensions/servers/gin/domains/fakes/upc.go
+++ extensions/servers/gin/domains/fakes/upc.go
@@ -5,9 +5,14 @@ import (
 	"github.com/vortex14/gotyphoon/elements/forms"
 	"github.com/vortex14/gotyphoon/elements/models/label"
 	"github.com/vortex14/gotyphoon/extensions/data/fake"
-	GraphExtension "github.com/vortex14/gotyphoon/extensions/forms/graph"
+
 	GinExtension "github.com/vortex14/gotyphoon/extensions/servers/gin"
+
+	// /* ignore for building amd64-linux
+	GraphExtension "github.com/vortex14/gotyphoon/extensions/forms/graph"
 	GinGraphExt "github.com/vortex14/gotyphoon/extensions/servers/gin/graph"
+	// */
+
 	"github.com/vortex14/gotyphoon/interfaces"
 )
 
