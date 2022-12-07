7946472f28d49343e71764be43f322a362141b23
Dmitriy <d.a.viharev@gmail.com>
2022-12-07 16:56:08 +1000

    updated

--- extensions/pipelines/http/net-http/middlewares.go
+++ extensions/pipelines/http/net-http/middlewares.go
@@ -1,14 +1,14 @@
 package net_http
 
 import (
+	"context"
+	b64 "encoding/base64"
+	"errors"
 	Gin "github.com/gin-gonic/gin"
 	"github.com/vortex14/gotyphoon/elements/forms"
 	"github.com/vortex14/gotyphoon/elements/models/label"
 	"github.com/vortex14/gotyphoon/extensions/servers/gin"
 	"github.com/vortex14/gotyphoon/interfaces"
-
-	"context"
-	"errors"
 	"net/url"
 )
 
@@ -27,17 +27,36 @@ var UrlRequiredMiddleware = &gin.RequestMiddleware{
 	},
 	Fn: func(context context.Context, ctx *Gin.Context, logger interfaces.LoggerInterface, reject func(err error), next func(ctx context.Context)) {
 		params := ctx.Request.URL.Query()
+
 		sURL := params["url"]
+		encodeFlag := params["encode"]
+		isBase64 := false
+
+		if len(encodeFlag) > 0 && encodeFlag[0] == "base64" {
+			isBase64 = true
+		}
 
 		if len(sURL) == 0 {
-			logger.Error(sURL)
+			logger.Error(params.Encode())
 			reject(UrlError)
 			return
 		}
+		URL := sURL[0]
+		if isBase64 {
 
-		v, err := url.Parse(sURL[0])
+			sDec, be := b64.StdEncoding.DecodeString(URL)
+			if be != nil {
+				logger.Error(URL)
+				reject(UrlError)
+				return
+			}
+			URL = string(sDec)
+			logger.Warning(URL)
+		}
+
+		v, err := url.Parse(URL)
 		if err != nil || len(v.Host) == 0 {
-			logger.Error(sURL)
+			logger.Error(URL)
 			reject(UrlInvalidError)
 			return
 		}
